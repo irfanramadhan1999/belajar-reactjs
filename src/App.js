5acae1f07e581468163783390838294179f77fa1
irfanramadhan1999 <irfanramadhani@students.amikom.ac.id>
2023-02-16 22:19:09 +0700

    bug fixing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 
 // Apa Itu Object & State React Js | Study With Me Eps.11
 class App extends React.Component {
-
+    render() {
+        return(
+            <div></div>
+        )
+    }
 }
 
 
